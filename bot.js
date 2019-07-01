16a761c5bc8064d98cb23a4472c66c29df82d00b
threl <threl.d.9@gmail.com>
2019-07-01 12:31:50 +0700

    Initial commit

---
+++ bot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const auth = require('./auth.json');
+const PREFIX = '!';
+
+bot.on('ready', () => {
+    console.log(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.login(auth.token);
+
+bot.on('message', message => {
+
+  let args = message.content.substring(PREFIX.length).split(" ");
+
+  let cmd = args[0];
+
+  switch (cmd) {
+    case 'leek':
+      message.channel.send('Ada yang mau league @everyone?');
+      break;
+    default:
+      break;
+  }
+});
